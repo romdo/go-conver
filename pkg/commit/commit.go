277f7ea311e99484ff82cc5d6331af75640373c4
Jim Myhrberg <contact@jimeh.me>
2020-11-01 16:22:27 +0000

    wip: update exposed structs in preparation for whole commit parsing

--- pkg/commit/commit.go
+++ pkg/commit/commit.go
@@ -3,16 +3,22 @@
 package commit
 
 type Commit struct {
-	Type       string
-	Scope      string
-	Subject    string
-	Body       string
-	Footers    []*Footer
-	IsBreaking bool
+	Type            string
+	Scope           string
+	Subject         string
+	Body            string
+	Footers         []*Footer
+	References      []*Reference
+	BreakingChanges []string
+	IsBreaking      bool
 }
 
 type Footer struct {
-	Name      string
-	Body      string
-	Reference bool
+	Name  string
+	Value string
+}
+
+type Reference struct {
+	Name  string
+	Value string
 }
